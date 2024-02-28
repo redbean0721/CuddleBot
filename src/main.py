9f76b30cf03a37c96d8cc3b5054e9ff7ca8b51e0
redbean0721 <redbean0721gmail.com>
2024-02-28 08:19:20 +0800

    Add logging

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from dotenv import load_dotenv
 import discord
+import logging
 import json
 import os
 
+from modules.log import setup_logging
+
+setup_logging()
 load_dotenv()
 
 version = json.load(open("version.json", "r"))["version"]
@@ -12,7 +16,8 @@ bot = discord.Client(intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print(f"Logged in: {bot.user} | {bot.user.id}\nVersion: {version}")
+    logging.info(f"Logged in: {bot.user} | {bot.user.id}")
+    logging.info(f"Version: {version}")
 
 
 bot.run(os.getenv("TOKEN"))
---
+++ src/modules/log.py
@@ -0,0 +1,39 @@
+from logging.handlers import TimedRotatingFileHandler
+import datetime
+import logging
+import os
+
+
+def setup_logging():
+    logging.basicConfig(level=logging.INFO)
+    log_format = "[%(asctime)s \033[92m%(levelname)s]\033[0m: %(message)s"
+    date_format = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter(fmt=log_format, datefmt=date_format)
+
+    # 創建 FileHandler 並設置格式
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+
+    today_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    log_number = 1
+    while os.path.exists(f"logs/{today_date}-{log_number}.log"):
+        log_number += 1
+    log_filename = f"logs/{today_date}-{log_number}.log"
+    log_handler = TimedRotatingFileHandler(filename=log_filename, when="midnight", interval=1, backupCount=7)
+    log_handler.suffix = "%Y-%m-%d.log"  # 文件名後墜
+
+    file_handler = logging.FileHandler(log_filename)
+    file_handler.setFormatter(formatter)
+
+    # 獲取根logger
+    root_logger = logging.getLogger()
+
+    # 移除根logger上的所有現有處理程序(handlers)
+    for handler in root_logger.handlers:
+        root_logger.removeHandler(handler)
+
+    # 添加新的 StreamHandler 和 FileHandler
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    root_logger.addHandler(stream_handler)
+    root_logger.addHandler(file_handler)
